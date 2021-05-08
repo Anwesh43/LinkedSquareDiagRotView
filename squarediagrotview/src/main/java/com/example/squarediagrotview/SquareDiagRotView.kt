1c7fde487b8388f60e65d1524a55fc72d88d4866
Anwesh43 <anweshthecool0@gmail.com>
2021-05-08 11:04:56 +0530

    adding Canvas extension methods

--- squarediagrotview/src/main/java/com/example/squarediagrotview/SquareDiagRotView.kt
+++ squarediagrotview/src/main/java/com/example/squarediagrotview/SquareDiagRotView.kt
@@ -29,3 +29,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *  n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareDiagRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2 + (w / 2 - size) * sf2, 0f)
+        drawRect(RectF(0f, -size / 2, size * sf1, size / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSquareDiagRot(scale, w, h, paint)
+}
