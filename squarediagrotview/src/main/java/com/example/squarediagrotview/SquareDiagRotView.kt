9d464ccce062cd5793a418b935772de54a194d9e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-08 10:53:05 +0530

    adding constants and libraries

--- squarediagrotview/src/main/java/com/example/squarediagrotview/SquareDiagRotView.kt
+++ squarediagrotview/src/main/java/com/example/squarediagrotview/SquareDiagRotView.kt
@@ -1,2 +1,26 @@
 package com.example.squarediagrotview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#004D40",
+    "#FF6F00",
+    "#0091EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 3.9f
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
